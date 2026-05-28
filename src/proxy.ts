b3963cc34b40ed7aa4573101984035b26b477392
Owen <owen@txv.io>
2026-05-27 21:31:59 -0700

    Middleware -> proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { build } from "@server/build";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     return NextResponse.next();
 }
 
