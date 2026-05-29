c1ef5b4fbef06210083aaf8ad2d62fd11f396f55
Owen <owen@txv.io>
2026-05-28 21:23:55 -0700

    Add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,29 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import fs from "fs";
+import path from "path";
 
 const withNextIntl = createNextIntlPlugin();
+// read allowedDevOrigins.json if it exists
+let allowedDevOrigins: string[] = [];
+const allowedDevOriginsPath = path.join(
+    process.cwd(),
+    "allowedDevOrigins.json"
+);
+if (fs.existsSync(allowedDevOriginsPath)) {
+    try {
+        const data = fs.readFileSync(allowedDevOriginsPath, "utf-8");
+        allowedDevOrigins = JSON.parse(data);
+        console.log("Loaded allowed development origins:", allowedDevOrigins);
+    } catch {}
+}
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
     reactCompiler: true,
     transpilePackages: ["@novnc/novnc"],
-    output: "standalone"
+    output: "standalone",
+    allowedDevOrigins
 };
 
 export default withNextIntl(nextConfig);
