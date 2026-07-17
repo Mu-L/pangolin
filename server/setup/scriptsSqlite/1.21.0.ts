a0b9ea76a3ee5495511f76c3640f95d7da0651b3
miloschwartz <mschwartz10612@gmail.com>
2026-07-17 16:22:15 -0400

    add badger to migration

--- server/setup/scriptsSqlite/1.21.0.ts
+++ server/setup/scriptsSqlite/1.21.0.ts
@@ -1,6 +1,10 @@
 import { APP_PATH } from "@server/lib/consts";
 import Database from "better-sqlite3";
+import fs from "fs";
+import yaml from "js-yaml";
 import path from "path";
+import z from "zod";
+import { fromZodError } from "zod-validation-error";
 
 const version = "1.21.0";
 
@@ -51,6 +55,49 @@ export default async function migration() {
     } catch (e) {
         console.log("Failed to migrate db:", e);
         throw e;
+    }
+
+    try {
+        const traefikPath = path.join(
+            APP_PATH,
+            "traefik",
+            "traefik_config.yml"
+        );
+
+        const schema = z.object({
+            experimental: z.object({
+                plugins: z.object({
+                    badger: z.object({
+                        moduleName: z.string(),
+                        version: z.string()
+                    })
+                })
+            })
+        });
+
+        const traefikFileContents = fs.readFileSync(traefikPath, "utf8");
+        const traefikConfig = yaml.load(traefikFileContents) as any;
+
+        const parsedConfig = schema.safeParse(traefikConfig);
+
+        if (!parsedConfig.success) {
+            throw new Error(fromZodError(parsedConfig.error).toString());
+        }
+
+        traefikConfig.experimental.plugins.badger.version = "v1.5.0";
+
+        const updatedTraefikYaml = yaml.dump(traefikConfig);
+
+        fs.writeFileSync(traefikPath, updatedTraefikYaml, "utf8");
+
+        console.log(
+            "Updated the version of Badger in your Traefik configuration to v1.5.0"
+        );
+    } catch (e) {
+        console.log(
+            "We were unable to update the version of Badger in your Traefik configuration. Please update it manually. Check the release notes for this version for more information."
+        );
+        console.error(e);
     }
 
     console.log(`${version} migration complete`);
