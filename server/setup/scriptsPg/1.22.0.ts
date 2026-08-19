a7bbafd2f0d7438cc13129884d1709834c0af058
miloschwartz <mschwartz10612@gmail.com>
2026-08-19 12:01:01 -0400

    bump badger version

--- server/setup/scriptsPg/1.22.0.ts
+++ server/setup/scriptsPg/1.22.0.ts
@@ -451,14 +451,14 @@ export default async function migration() {
             throw new Error(fromZodError(parsedConfig.error).toString());
         }
 
-        traefikConfig.experimental.plugins.badger.version = "v1.6.0";
+        traefikConfig.experimental.plugins.badger.version = "v1.6.1";
 
         const updatedTraefikYaml = yaml.dump(traefikConfig);
 
         fs.writeFileSync(traefikPath, updatedTraefikYaml, "utf8");
 
         console.log(
-            "Updated the version of Badger in your Traefik configuration to v1.6.0"
+            "Updated the version of Badger in your Traefik configuration to v1.6.1"
         );
     } catch (e) {
         console.log(
