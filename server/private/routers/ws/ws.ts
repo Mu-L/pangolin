54bbe82504ece60fc2099f4e545b1917037b767d
Owen <owen@txv.io>
2026-09-04 09:06:28 -0400

    Dont log invalid message type
    Fixes #3695

--- server/private/routers/ws/ws.ts
+++ server/private/routers/ws/ws.ts
@@ -104,7 +104,8 @@ const processMessage = async (
 
         const handler = messageHandlers[message.type];
         if (!handler) {
-            throw new Error(`Unsupported message type: ${message.type}`);
+            logger.debug(`No handler found for message type: ${message.type}`);
+            return;
         }
 
         const response = await handler({
