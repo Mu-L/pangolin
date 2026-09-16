735787b18316cd032fc43e4d8fdd4be749728a30
Owen <owen@txv.io>
2026-09-16 10:11:20 -0400

    Fix #3778

--- server/setup/scriptsSqlite/1.23.0.ts
+++ server/setup/scriptsSqlite/1.23.0.ts
@@ -16,13 +16,13 @@ export default async function migration() {
         db.transaction(() => {
             db.prepare(
                 `
-            ALTER TABLE 'newt' ADD COLUMN IF NOT EXISTS 'agent' text;
+            ALTER TABLE 'newt' ADD COLUMN 'agent' text;
                 `
             ).run();
 
             db.prepare(
                 `
-            ALTER TABLE 'newt' ADD COLUMN IF NOT EXISTS 'agentVersion' text;
+            ALTER TABLE 'newt' ADD COLUMN 'agentVersion' text;
                 `
             ).run();
         })();
